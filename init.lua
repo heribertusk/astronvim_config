54d4a27a8714586ec80d52bd6e13f5ba6917c198
her14nto <her14nto@gmail.com>
2023-06-11 08:45:36 +0700

    update init.lua set auto format on save
    vue, js, lua, php

--- init.lua
+++ init.lua
@@ -35,6 +35,10 @@ return {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
+          "vue",
+          "js",
+          "php",
+          "lua"
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
