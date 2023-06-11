8c946cd8cd2223e18c841dbbd427136cad471b62
her14nto <her14nto@gmail.com>
2023-06-11 09:19:07 +0700

    update community.lua pack lua, vue, typescript

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,9 +3,11 @@ return {
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.color.tint-nvim" },
-  { import = "astrocommunity.media.vim-wakatime"},
+  { import = "astrocommunity.media.vim-wakatime" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.typescript" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
