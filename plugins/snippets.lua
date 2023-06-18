cdb768fc025de24242484fe50f369074e4385264
her14nto <her14nto@gmail.com>
2023-06-18 12:31:09 +0700

    lazyvim supertab recipes
    https://www.lazyvim.org/configuration/recipes#supertab

--- plugins/snippets.lua
+++ plugins/snippets.lua
@@ -2,8 +2,9 @@ return {
   {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
-      require "plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      require "plugins.configs.luasnip" (plugin, opts)                                       -- include the default astronvim config that calls the setup call
       require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets" } } -- load snippets paths
     end,
+    keys = function() return {} end,
   },
 }
