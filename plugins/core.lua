1b4ea72dcb37ec7d380a315314280cfe422e859f
her14nto <her14nto@gmail.com>
2023-06-21 12:17:54 +0700

    disable formatting for LSP Volar

--- plugins/core.lua
+++ plugins/core.lua
@@ -23,7 +23,7 @@ return {
       setup = {
         eslint = function()
           require("lazyvim.util").on_attach(function(client)
-            if client.name == "eslint" then
+            if client.name == "eslint_d" then
               client.server_capabilities.documentFormattingProvider = true
             elseif client.name == "tsserver" then
               client.server_capabilities.documentFormattingProvider = false
